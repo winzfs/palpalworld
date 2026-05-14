01aaef72852043f552bab54eb6caee4f63bba92f
winzfs <jazzhjm@gmail.com>
2026-05-15 00:43:24 +0900

    Remove duplicate floating minimap from home page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,12 +1,10 @@
 import { GameClientTileDemo } from "../features/game/GameClientTileDemo";
-import { FloatingMiniMap } from "../features/world/FloatingMiniMap";
 import { TileTravelBanner } from "../features/world/TileTravelBanner";
 
 export default function HomePage() {
   return (
     <>
       <GameClientTileDemo />
-      <FloatingMiniMap />
       <TileTravelBanner />
     </>
   );
