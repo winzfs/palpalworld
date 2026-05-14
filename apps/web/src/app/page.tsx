7aaabfbf4f873562544fbed3f72cf02986059d03
winzfs <jazzhjm@gmail.com>
2026-05-14 09:37:01 +0900

    Mount minimap independently from game panels

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,11 @@
 import { GameClient } from "../features/game/GameClient";
+import { FloatingMiniMap } from "../features/world/FloatingMiniMap";
 
 export default function HomePage() {
-  return <GameClient />;
+  return (
+    <>
+      <GameClient />
+      <FloatingMiniMap />
+    </>
+  );
 }
