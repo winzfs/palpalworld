303aba980861ce5d1bfb98d11d4d2513cbc6231a
winzfs <jazzhjm@gmail.com>
2026-05-14 17:49:24 +0900

    Show tile travel banner on home page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,11 +1,13 @@
 import { GameClientTileDemo } from "../features/game/GameClientTileDemo";
 import { FloatingMiniMap } from "../features/world/FloatingMiniMap";
+import { TileTravelBanner } from "../features/world/TileTravelBanner";
 
 export default function HomePage() {
   return (
     <>
       <GameClientTileDemo />
       <FloatingMiniMap />
+      <TileTravelBanner />
     </>
   );
 }
