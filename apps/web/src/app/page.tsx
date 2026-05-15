5134c04d56a10cdef35002fc3cd02086be912374
winzfs <jazzhjm@gmail.com>
2026-05-15 09:56:49 +0900

    Mount multiplayer overlay on game page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,12 @@
 import { GameClientTileDemoStation } from "../features/game/GameClientTileDemoStation";
+import { MultiplayerOverlay } from "../features/multiplayer/MultiplayerOverlay";
 import { TileTravelBanner } from "../features/world/TileTravelBanner";
 
 export default function HomePage() {
   return (
     <>
       <GameClientTileDemoStation />
+      <MultiplayerOverlay />
       <TileTravelBanner />
     </>
   );
